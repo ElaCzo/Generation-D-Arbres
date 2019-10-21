a476eecd966970e18a31a4785f638b8c6ec1a01d
ElisabethAbbasZadeh <elisabeth.abbaszadeh@gmail.com>
2019-10-21 11:29:37 +0200

    Implémentation du générateur linéaire congruentiel

--- Rand48.c
+++ Rand48.c
@@ -1,3 +1,14 @@
 long next(long a, long xn, long c, long m){
     return (a*xn+c)%m;
 }
+
+long* generateur(int nombre, long a, long xn, long c, long m){
+    long *res = malloc(sizeof(long)*nombre);
+    memset(res, 0, sizeof(long)*nombre);
+    
+    int i;
+    for(i=0; i<nombre; i++)
+        res[i]= next(a, xn, c, m);
+
+    return res;
+}
