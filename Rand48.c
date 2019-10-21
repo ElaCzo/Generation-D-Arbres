e5a82273f33de5028977d64821132b31793eed28
ElisabethAbbasZadeh <elisabeth.abbaszadeh@gmail.com>
2019-10-21 11:39:58 +0200

    Implémentation de rand48(), du générateur de Java et de l'affichage des entiers générés.

--- Rand48.c
+++ Rand48.c
@@ -2,13 +2,38 @@ long next(long a, long xn, long c, long m){
     return (a*xn+c)%m;
 }
 
-long* generateur(int nombre, long a, long xn, long c, long m){
+long* generateur(int nombre, long a, long x0, long c, long m){
     long *res = malloc(sizeof(long)*nombre);
     memset(res, 0, sizeof(long)*nombre);
     
     int i;
+    long xn =x0;
     for(i=0; i<nombre; i++)
-        res[i]= next(a, xn, c, m);
+        xn = res[i] = next(a, xn, c, m);
 
     return res;
+}
+
+long* rand48(int nombre, long a, long x0, long c){
+    return generateur(nombre, a, x0, c, (1<<48));
+}
+
+long* generateur_java(int nombre){
+    return rand48(nombre, 25214903917, 0, 11);
+}
+
+void affiche(long* entiers, int nombre){
+    int i;
+    for(i=0; i<nombre; i++){
+        printf("%l", entiers[i]);
+    }
+}
+
+int main(){
+    // calcul des 10 premier termes à partir de la graine 0 : 
+    int nombre = 10;
+    long *alea;
+    affiche(alea=generateur_java(nombre), nombre);
+
+    free(alea);
 }
